3bf9fd1ed7c96d94a1a7dc0c16df1c6bd5db3178
thunderbiscuit <thunderbiscuit@protonmail.com>
2023-05-25 12:58:45 -0400

    Migrate docs to VitePress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "Uniffi-Examples",
+  description: "",
+  base: "/uniffi-examples/",
+  themeConfig: {
+    search: {
+      provider: "local"
+    },
+
+    sidebar: [
+      // Getting Started
+      {
+        text: "Getting Started",
+        collapsed: false,
+        items: [
+          { text: "Introduction", link: "/getting-started/introduction.md" },
+          { text: "Languages", link: "/getting-started/languages.md" },
+        ]
+      },
+
+      // Examples
+      {
+        text: "Examples",
+        collapsed: false,
+        items: [
+          { text: "Functions", link: "/examples/functions.md" },
+          { text: "Enums", link: "/examples/enums.md" },
+          { text: "Structs", link: "/examples/structs.md" },
+          { text: "Objects", link: "/examples/objects.md" },
+        ]
+      },
+    ],
+  },
+})
--- docs/examples/functions.md
+++ docs/examples/functions.md
@@ -1,6 +1,6 @@
 # Functions
 
-The [functions example]([1-functions/](https://github.com/thunderbiscuit/uniffi-examples/tree/master/1-functions)) demonstrates how to expose root-level functions.
+The [functions example](https://github.com/thunderbiscuit/uniffi-examples/tree/master/1-functions) demonstrates how to expose root-level functions.
 
 Note that the `namespace` block _must_ be present in the UDL file, and that root-level functions are defined in it like so:
 ```uniffi-udl
